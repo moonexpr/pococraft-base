667a649981d7dcdc0ed219e26dd11540525d501d
Potatofactory <potatofactory@outlook.com>
2015-08-18 20:08:25 -0500

    Fixed Capitalization (p2)

---
+++ pococraft_base/modules/all/chat_welcometext/init.lua
@@ -0,0 +1,31 @@
+ModInfo.Name = "Welcome Announcement"
+ModInfo.Author = "Potatofactory, Mr. Floppy Disk"
+
+function TestSuccess()
+	return true
+end
+
+ServerName = {
+	27015 = "Club Random",
+	27120 = "Christmas Town (Developer Server)"
+}
+
+hook.Add( "PlayerInitialSpawn", "AnnouncePlayer", function( player_object )
+	if player_object:IsAdmin() and player_object:IsValid() then
+		PrintMessage( HUD_PRINTTALK, string.format("<avatar=%s> <hsv>%s (Staff Member)</hsv> has entered <c=200,0,100>%s</c>", player_object:SteamID(), player_object:Nick(), ServerName[GetConVar("hostport"):GetInt()] ) )
+	else
+		PrintMessage( HUD_PRINTTALK, string.format("<avatar=%s> <hsv>%s (%s)</hsv> has entered <c=200,0,100>%s</c>", player_object:SteamID(), player_object:Nick(), player_object:SteamID(), ServerName[GetConVar("hostport"):GetInt()] ) )
+	end
+	local sound = "surface.PlaySound(\"garrysmod/save_load" .. math.random(1, 4) .. ".wav\")"
+	for _, ply in pairs(player.GetAll()) do
+		ply:SendLua( sound )
+	end
+end )
+
+hook.Add( "PlayerDisconnected", "AnnouncePlayer", function( player_object )
+	if player_object:IsAdmin() and player_object:IsValid() then
+		PrintMessage( HUD_PRINTTALK, string.format("<avatar=%s> <c=0,200,200>%s</c> has left <c=200,0,100>%s</c>", player_object:SteamID(), player_object:Nick(), ServerName[GetConVar("hostport"):GetInt()] ) )
+	else
+		PrintMessage( HUD_PRINTTALK, string.format("<avatar=%s> <c=0,150,150>%s</c> has left <c=200,0,100>%s</c>", player_object:SteamID(), player_object:Nick(), ServerName[GetConVar("hostport"):GetInt()] ) )
+	end
+end )
